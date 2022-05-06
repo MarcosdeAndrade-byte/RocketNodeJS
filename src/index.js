7dc5f9512ac53be0c2dc449c193befaad8bad473
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-06 13:56:40 -0300

    Criando metodos de requisicao e adicionando nodemon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response, request } = require("express");
 const express = require("express");
 
 const app = express();
@@ -5,7 +6,23 @@ const app = express();
 //Localhost: 3333
 
 app.get("/",(request,response)=> {
-    return response.json({mensagem: "Hello"});
+    return response.json(["Curso 1","Curso 2","Curso 3"]);
+});
+
+app.post("/courses",(request,response)=> {
+    return response.json(["Curso 1","Curso 2","Curso 3"]);
+});
+
+app.put("/courses/:id",(request,response)=> {
+    return response.json(["Curso 6","Curso 2","Curso 3"]);
+});
+
+app.patch("/courses/:id",(request,response)=>{
+    return response.json(["Curso 1","Curso 7","Curso 3"]);
+});
+
+app.delete("/course/:id",(response,request)=>{
+    return response.json(["Curso 1","Curso 7","Curso 3"]);
 });
 
 app.listen(3333);
