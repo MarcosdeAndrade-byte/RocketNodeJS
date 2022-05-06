3d14e5003e15e50cfc8c8d965c16f3264ff0df59
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-06 16:35:04 -0300

    Tipos de parametros

--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@ const express = require("express");
 
 const app = express();
 
+
+app.use(express.json());
+
 //Localhost: 3333
 
-app.get("/",(request,response)=> {
+app.get("/courses",(request,response)=> {
+    const query = request.query;
+    console.log(query);
     return response.json(["Curso 1","Curso 2","Curso 3"]);
 });
 
 app.post("/courses",(request,response)=> {
-    return response.json(["Curso 1","Curso 2","Curso 3"]);
+    const body = request.body;
+    console.log(body);
+     return response.json(["Curso 1","Curso 2","Curso 3"]);
 });
 
 app.put("/courses/:id",(request,response)=> {
     return response.json(["Curso 6","Curso 2","Curso 3"]);
 });
 
-app.patch("/courses/:id",(request,response)=>{
+app.patch("/courses/:id",(request,response)=> {
     return response.json(["Curso 1","Curso 7","Curso 3"]);
 });
 
-app.delete("/course/:id",(response,request)=>{
+app.delete("/courses/:id",(request,response)=> {
     return response.json(["Curso 1","Curso 7","Curso 3"]);
 });
 
