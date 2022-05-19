8bf5a85b276fbbac413844c2ba54276a63237f3b
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-05-19 18:29:37 -0300

    Mudancas de codigo para atender os testes da API

--- ignite-template-corrigindo-o-codigo/src/index.js
+++ ignite-template-corrigindo-o-codigo/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate: uuidValidate } = require("uuid");
 
 const app = express();
 
@@ -8,47 +8,64 @@ app.use(express.json());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
-
-app.post("/repositories", (request, response) => {
+function creationChecker(request, response ,next){
   const { title, url, techs } = request.body
 
-  const repository = {
-    id: uuid(),
-    title,
-    url,
-    techs,
-    likes: 0
-  };
+  if(!Array.isArray(techs) || url.indexOf(title) == -1){
+    return response.status(404).send()
+  }
 
-  return response.json(repository);
+  return next()
+}
+
+//Pegar todos os repositórios 
+app.get("/repositories", (request, response) => {
+  return response.status(201).json(repositories);
+});
+
+//Criar um repositório
+app.post("/repositories",creationChecker,(request, response) => {
+  const { title, url, techs } = request.body;
+
+  repositories.push({
+    id: uuid(),
+    title : title,
+    url : url,
+    techs : techs,
+    likes: 0
+  });
+
+  return response.status(201).json(repositories).send();
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const updatedRepository = request.body;
+  const id  = request.query['id'];
+  const {title,url,techs} = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  //Lógica para encontrar o elemento do repositório certo
+  const repo = repositories.find((repo,index) => repo.id === id);
+
+  if(repo == undefined){
+    return response.status(404).json("Repositório não encontrado").end()
+  }else if(title != undefined){
+    repo.title = title;
+  }else if(url != undefined){
+    repo.url = url;
+  }else if(techs != undefined){
+    repo.techs = techs;
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
-
-  return response.json(repository);
+  return response.status(201).json(repositories).end();
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+  const { id }  = request.params;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex == -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,15 +77,15 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex == -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
--- ignite-template-corrigindo-o-codigo/src/server.js
+++ ignite-template-corrigindo-o-codigo/src/server.js
@@ -1,3 +1,5 @@
 const app = require("./");
 
-app.listen(3333);
+app.listen(3000,() => {
+    console.log("Server is running on port " + 3000);
+});
