294bda352303dc6a23042220dfada8377139341f
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-06-01 14:53:40 -0300

    Conhecendo o conceito de stream

--- Typescript/CriandoAPINodeJs/src/modules/cars/useCases/ImportCategoryUseCase/ImportCategoryUseCase.ts
+++ Typescript/CriandoAPINodeJs/src/modules/cars/useCases/ImportCategoryUseCase/ImportCategoryUseCase.ts
@@ -1,6 +1,22 @@
+import fs from 'fs';
+
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        // Criamos uma stream
+        const stream = fs.createReadStream(file.path);
+
+        // criamos um csv-parse que vai ler linha por linha do arquivo
+        const parseFile = parse();
+
+        // Passamos os dados da stream para variável parseFile
+        stream.pipe(parseFile);
+
+        // utilizamos criamos uma função para mostrar os dados da stream
+        parseFile.on('data', async line => {
+            console.log(line);
+        });
     }
 }
 
