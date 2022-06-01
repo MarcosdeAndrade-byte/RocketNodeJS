68ea3b2b37b7e56a45a76925ae487f566065a69a
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-06-01 09:46:28 -0300

    Refatorando as rotas

--- Typescript/CriandoAPINodeJs/src/server.ts
+++ Typescript/CriandoAPINodeJs/src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specification.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-// Ao invés de repetir o patch(categories) nos métodos,podemos adicionar ele diretamente no middleware
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+// Dedicamos o arquivo index.ts para armazenar as rotas da nossa aplicação
+app.use(router);
 
 app.listen(3333, () => console.log('Servidor rodando'));
