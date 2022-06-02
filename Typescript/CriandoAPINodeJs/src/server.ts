fa335fd22e136caa8cbeb96ce234dc90d043a378
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-06-02 10:50:17 -0300

    Configurando o swagger

--- Typescript/CriandoAPINodeJs/src/server.ts
+++ Typescript/CriandoAPINodeJs/src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 // Dedicamos o arquivo index.ts para armazenar as rotas da nossa aplicação
 app.use(router);
 
-app.listen(3333, () => console.log('Servidor rodando'));
+app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
